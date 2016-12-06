292c35571dc94ab08a76032a437a0eaf14ab8c61
Mats Högberg <mats@hgbrg.se>
2016-12-06 23:00:25 +0100

    Solutions for day 5, parts 1 and 2

---
+++ Day 5 - How About a Nice Game of Chess?/Solution1.hs
@@ -0,0 +1,27 @@
+import Data.Maybe
+import Data.Char
+import Data.String.Utils
+import Data.Digest.Pure.MD5
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as BLazy
+import qualified Data.ByteString.Lazy.Char8 as CLazy
+
+getPassword :: String -> Int -> String
+getPassword str n = map (!! 5) $ firstNValidHashes str n
+
+firstNValidHashes :: String -> Int -> [String]
+firstNValidHashes str n = firstNValidHashes' str 0 n
+
+firstNValidHashes' :: String -> Int -> Int -> [String]
+firstNValidHashes' _ _ 0 = []
+firstNValidHashes' str i n = hash : firstNValidHashes' str (i' + 1) (n - 1)
+  where (i', hash) = firstValidHash str i
+
+firstValidHash :: String -> Int -> (Int, String)
+firstValidHash str i | startswith "00000" hash = (i, hash)
+                              | otherwise = firstValidHash str (i + 1)
+  where hash = md5' (str ++ show i)
+
+md5' :: String -> String
+md5' str = show . md5 $ CLazy.pack str
