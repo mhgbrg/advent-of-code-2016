a270f985876defebaec6b07f3f94ade5bcc2dbaa
Mats Högberg <mats@hgbrg.se>
2016-12-06 23:43:11 +0100

    Solved day 6, part 1

---
+++ Day 6 - Signals and Noise/Solution1.hs
@@ -0,0 +1,24 @@
+import Data.List
+
+main = do
+  input <- readFile "input.txt"
+  let solution = solve $ lines input
+  putStrLn solution
+
+solve :: [String] -> String
+solve lines = map mostFrequentChar columns
+  where columns = transpose lines
+
+mostFrequentChar :: String -> Char
+mostFrequentChar str = fst $ head sortedCounts
+  where counts = charCounts str
+        sortedCounts = sortBy compareCounts counts
+        compareCounts (_, n1) (_, n2) = n2 `compare` n1
+
+charCounts :: String -> [(Char, Int)]
+charCounts str = map (\c -> (c, countOccurrences' c)) uniqueChars
+  where uniqueChars = nub str
+        countOccurrences' c = countOccurrences c str
+
+countOccurrences :: Char -> String -> Int
+countOccurrences c = length . filter (== c)
