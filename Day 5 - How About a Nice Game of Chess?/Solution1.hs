0ef8221b8db2aa0ac9141d411023277c45c54bf2
Mats Högberg <mats@hgbrg.se>
2016-12-06 23:05:52 +0100

    Removed unused exports

--- Day 5 - How About a Nice Game of Chess?/Solution1.hs
+++ Day 5 - How About a Nice Game of Chess?/Solution1.hs
@@ -1,10 +1,6 @@
-import Data.Maybe
 import Data.Char
 import Data.String.Utils
 import Data.Digest.Pure.MD5
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Lazy as BLazy
 import qualified Data.ByteString.Lazy.Char8 as CLazy
 
 getPassword :: String -> Int -> String
--- Day 5 - How About a Nice Game of Chess?/Solution2.hs
+++ Day 5 - How About a Nice Game of Chess?/Solution2.hs
@@ -1,12 +1,8 @@
-import Data.Maybe
 import Data.Char
 import Data.List
 import Data.Ord
 import Data.String.Utils
 import Data.Digest.Pure.MD5
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Lazy as BLazy
 import qualified Data.ByteString.Lazy.Char8 as CLazy
 
 getPassword :: String -> Int -> String
